5be3aedbabae144b9041914f50a621a622a25351
Krisz <bkrisz.100@gmail.com>
2021-12-01 16:43:39 +0100

    Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    public Button btnPlus;
+    @FXML
+    public Button btnMinus;
+    @FXML
+    public Button btnMulti;
+    @FXML
+    public Button btnDiv;
+    @FXML
+    public Button btnPerc;
+    @FXML
+    public Label lblNum;
+    @FXML
+    public Spinner txtFirst;
+    @FXML
+    public Spinner txtSecond;
+
+    @FXML
+    public void plusClick(ActionEvent actionEvent) {
+       
+    }
+
+    @FXML
+    public void minusClick(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void multiClick(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void divClick(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void percClick(ActionEvent actionEvent) {
+
+    }
 }
