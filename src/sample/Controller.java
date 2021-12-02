3ac638d40de51216f39958d222f9b522c5ee0af2
Krisz <bkrisz.100@gmail.com>
2021-12-02 11:55:26 +0100

    Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 
 import java.awt.*;
@@ -9,44 +10,70 @@ import java.awt.*;
 public class Controller {
 
     @FXML
-    public Button btnPlus;
-    @FXML
-    public Button btnMinus;
-    @FXML
-    public Button btnMulti;
-    @FXML
-    public Button btnDiv;
-    @FXML
-    public Button btnPerc;
-    @FXML
     public Label lblNum;
     @FXML
-    public Spinner txtFirst;
+    public Spinner<Double> txtFirst;
     @FXML
-    public Spinner txtSecond;
+    public Spinner<Double> txtSecond;
 
     @FXML
-    public void plusClick(ActionEvent actionEvent) {
-       
+    public void plusClick() {
+        if (txtFirst.getValue() == 0 || txtSecond.getValue() == 0) {
+            lblNum.setText("Nem lehet nulla");
+        }
+        else {
+            double num1 = txtFirst.getValue();
+            double num2 = txtSecond.getValue();
+            lblNum.setText(String.valueOf(num1 + num2));
+        }
     }
 
     @FXML
-    public void minusClick(ActionEvent actionEvent) {
-
+    public void minusClick() {
+        if (txtFirst.getValue() == 0 || txtSecond.getValue() == 0) {
+            lblNum.setText("Nem lehet nulla");
+        }
+        else {
+            double num1 = txtFirst.getValue();
+            double num2 = txtSecond.getValue();
+            lblNum.setText(String.valueOf(num1 - num2));
+        }
     }
+
 
     @FXML
     public void multiClick(ActionEvent actionEvent) {
-
+        if (txtFirst.getValue() == 0 || txtSecond.getValue() == 0) {
+            lblNum.setText("Nem lehet nulla");
+        }
+        else {
+            double num1 = txtFirst.getValue();
+            double num2 = txtSecond.getValue();
+            lblNum.setText(String.valueOf(num1 * num2));
+        }
     }
 
     @FXML
     public void divClick(ActionEvent actionEvent) {
-
+        if (txtFirst.getValue() == 0 || txtSecond.getValue() == 0) {
+            lblNum.setText("Nem lehet nulla");
+        }
+        else {
+            double num1 = txtFirst.getValue();
+            double num2 = txtSecond.getValue();
+            lblNum.setText(String.valueOf(num1 / num2));
+        }
     }
 
     @FXML
     public void percClick(ActionEvent actionEvent) {
-
+        if (txtFirst.getValue() == 0 || txtSecond.getValue() == 0) {
+            lblNum.setText("Nem lehet nulla");
+        }
+        else {
+            double num1 = txtFirst.getValue();
+            double num2 = txtSecond.getValue();
+            lblNum.setText(String.valueOf(num1 / num2));
+        }
     }
 }
